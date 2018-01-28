fe5ffaf8434797aaafc6e87e659ebc4e34e91b6f
Ciaran79 <ciarankamp@gmail.com>
2018-01-28 20:53:23 +0000

    latest

--- scripts.js
+++ scripts.js
@@ -4,21 +4,17 @@ function getQuote() {
     var quoteAuthorArea = document.getElementById('quote-author');
 
     xhr.open('GET', 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=10', true);
-
     xhr.setRequestHeader('X-Mashape-Key', 'OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V');
-
- 
     xhr.onload = function () {
         if (xhr.status == 200) {
             var parsed = JSON.parse(this.responseText);
-console.log(parsed);
+            // console.log(parsed);
             quoteArea.innerHTML = parsed.quote;
             quoteAuthorArea.innerHTML = parsed.author;
 
         } else if (this.status == 403) {
             quoteArea.innerHTML = "There was a problem..";
         }
-
     };
     xhr.send();
 }
@@ -33,10 +29,10 @@ function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+}
 
-  document.getElementById('get-quote').addEventListener('click', getQuote);
-  document.getElementById('get-quote').addEventListener('click', changeBackground);
+document.getElementById('get-quote').addEventListener('click', getQuote);
+document.getElementById('get-quote').addEventListener('click', changeBackground);
